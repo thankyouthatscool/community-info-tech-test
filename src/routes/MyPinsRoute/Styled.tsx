5a4c3703d95bcc8716c8d60a16aaa784b06c4ba9
OZahnitko <ozahnitko@teleresult.com.au>
2022-07-05 11:03:48 +1000

    Better my pins.

--- src/routes/MyPinsRoute/Styled.tsx
+++ src/routes/MyPinsRoute/Styled.tsx
@@ -1,5 +1,16 @@
+import { Card } from "@mui/material";
 import styled from "styled-components";
 
 export const ContentWrapper = styled.div`
   flex: 1;
+
+  overflow: hidden auto;
+
+  padding: 0.5rem;
+`;
+
+export const StyledCard = styled(Card)`
+  margin-bottom: 0.5rem;
+
+  padding: 1rem;
 `;
